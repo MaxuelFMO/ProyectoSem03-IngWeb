648d79f90a8c42cf9d06f97f45453149bd6921ab
Francis Maxuel U. O. <74038367@continental.edu.pe>
2026-04-07 21:57:50 -0500

    add eliminar

--- Crud/Eliminar.js
+++ Crud/Eliminar.js
@@ -1,7 +1,34 @@
 const { connectDB } = require('../Back/Connection');
 
 async function eliminarVenta(id) {
-  const connection = await connectDB();
+  let connection;
+  try {
+    connection = await connectDB();
+    if (!id) {
+      throw new Error('ID de venta requerido');
+    }
+
+    const query = 'DELETE FROM ventas WHERE id = ?';
+    const [result] = await connection.execute(query, [id]);
+
+    if (result.affectedRows === 0) {
+      throw new Error('Venta no encontrada');
+    }
+
+    console.log('Venta eliminada con ID:', id);
+    return result;
+  } catch (err) {
+    console.error('Error al eliminar venta:', err.message);
+    throw err;
+  } finally {
+    if (connection) {
+      try {
+        await connection.end();
+      } catch (e) {
+        console.error('Error al cerrar conexión:', e.message);
+      }
+    }
+  }
 }
 
 module.exports = { eliminarVenta };
